9de65c2493476317934f4d50c6bcb492748d7174
Victoria Shulman <vmshulm@victorias-macbook-pro.local>
2018-12-13 10:34:00 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Victoria Shulman on 12/13/18.
+//  Copyright © 2018 Victoria Shulman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
